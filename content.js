f83be23aa46c63b142c389d64ccd547fa1ddd2d0
Mudit Mahajan <muditmahajan21@gmail.com>
2023-03-07 17:39:23 +0530

    Send message successfully

--- content.js
+++ content.js
@@ -1,39 +1,26 @@
 (()=> {
-
-async function fill(selector, input) {
-  return new Promise(async function (resolve, reject) {
-    try {
-      let inputBox = await tab.findElement(
-        swd.By.css(
-          selector
-        )
-      );
-      await inputBox.sendKeys(input);
-      await tab;
-      resolve();
-    } catch (error) {
-      reject();
-    }
-  })
-}
-
-const hide = (elem) => {
-  elem.classList.remove("visible");
-  elem.classList.remove("hidden");	// Add hidden class only if it is not there in classList of element
-  elem.classList.add("hidden");
-};
-
 chrome.runtime.onMessage.addListener(async (obj, sender, response) => {  
   try {
     if(obj.message === "addMessage") {  
       const messageButton = document.getElementsByClassName("message-anywhere-button pvs-profile-actions__action artdeco-button")[0];
       if(messageButton) {
         await messageButton.click();
+        // Wait for the message box to appear
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const messageBox = document.getElementsByClassName("msg-form__contenteditable")[0];
+        if(messageBox) {       
+          
+          messageBox.innerHTML = "<p><br/>" + obj.value + "</p>";
+          messageBox.dispatchEvent(new Event('input', {bubbles: true}));
 
-        // Find the Send Button and click it
-        const sendButton = document.getElementsByClassName("msg-form__send-button");
-        if(sendButton) {
-          await sendButton[0].click();
+          // Find the Send Button and click it
+          const sendButton = document.getElementsByClassName("msg-form__send-button");
+          if(sendButton) {
+            sendButton[0].disabled = false;
+            await sendButton[0].click();
+          }
+        } else {
+          console.log("Message box not found");
         }
       }
     }   
