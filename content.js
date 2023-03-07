dc6dd0ce9f77c3b21e2b2f880267a535df3e6c7f
Mudit Mahajan <muditmahajan21@gmail.com>
2023-03-07 12:31:54 +0530

    Add send button

--- content.js
+++ content.js
@@ -25,27 +25,17 @@ const hide = (elem) => {
 
 chrome.runtime.onMessage.addListener(async (obj, sender, response) => {  
   try {
-  if(obj.message === "addMessage") {
-    // Find the linkedin Message Button in the profile
-    const messageButton = document.getElementsByClassName("message-anywhere-button pvs-profile-actions__action artdeco-button")[0];
-    if(messageButton) {
-      await messageButton.click();
-      
-      // Find the Message Box and fill it
-      const messageBox = document.getElementsByClassName("msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate")[0];
-      if(messageBox) {
-        await messageBox.click();
-        await messageBox.sendKeys("Hello, I am interested in your profile. Please accept my request.");
-      }
+    if(obj.message === "addMessage") {  
+      const messageButton = document.getElementsByClassName("message-anywhere-button pvs-profile-actions__action artdeco-button")[0];
+      if(messageButton) {
 
-      // Find the Send Button and click it
-      const sendButton = document.getElementsByClassName("msg-form__send-button");
-      if(sendButton) {
-        await sendButton[0].click();
+        // Find the Send Button and click it
+        const sendButton = document.getElementsByClassName("msg-form__send-button");
+        if(sendButton) {
+          await sendButton[0].click();
+        }
       }
-    }
-    console.log("Message Button Clicked");
-  }   
+    }   
   } catch(error) {
     console.log(error);
   }                             //list of all serach results
